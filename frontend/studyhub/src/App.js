398caae293df877c2046d303cb06d2084b798c24
cme0145 <carolinaestrada@my.unt.edu>
2025-12-04 17:22:52 -0600

    Deployed the website and added working signup/login functionality

--- frontend/studyhub/src/App.js
+++ frontend/studyhub/src/App.js
@@ -1,11 +1,12 @@
 import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Signup />
+      <Signup /> <Login />
     </div>
   );
 }

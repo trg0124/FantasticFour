0dfb5aa43c6ffc178c858fdf0f2e2f90a93ff1fa
km381 <KennediMack@my.unt.edu>
2025-12-07 21:11:52 -0600

    Started to implement edit button

--- frontend/studyhub/src/pages/Account Settings.jsx
+++ frontend/studyhub/src/pages/Account Settings.jsx
@@ -2,3 +2,38 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../auth/AuthContext";
 
+export default function Account() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const Row = ({ label, value, link }) => (
+    <div
+      style= {{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "1rem 0",
+         borderBottom: "1px solid #e5e5e5",
+        }}
+    >
+      <div>
+        <p style={{ fontWeight: 600 }}>{label}</p>
+        <p style={{ marginTop: "4px", color: "#666" }}>{value}</p>
+      </div>
+       {/* Edit Button */}
+      <button
+        onClick={() => navigate(link)}
+        style={{
+          background: "#5A4AE3",
+          color: "white",
+          border: "none",
+          padding: "0.5rem 1rem",
+          borderRadius: "8px",
+          cursor: "pointer",
+          fontWeight: 500,
+        }}
+      >
+        Edit
+      </button>
+    </div>
+  );
+

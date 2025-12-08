bfe2f7ef67e7e6242a9a3b017da644669896b756
km381 <KennediMack@my.unt.edu>
2025-12-08 10:34:20 -0600

    Updated Account Settings.jsx

--- frontend/studyhub/src/pages/Account Settings.jsx
+++ frontend/studyhub/src/pages/Account Settings.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { useAuth } from "../auth/AuthContext";
 
 export default function Account() {

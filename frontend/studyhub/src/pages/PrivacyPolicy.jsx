82df6a0466c2eada765f2e2ceaf2e3b10c5be786
km381 <KennediMack@my.unt.edu>
2025-12-10 01:42:03 -0600

    Created and updated PrivacyPolicy.jsx

---
+++ frontend/studyhub/src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,11 @@
+
+import { PageContainer } from "../components/SharedLayout";
+import "../styles/placeholders.css";
+
+export default function PrivacyPolicy() {
+  return (
+    <PageContainer title="Privacy Policy">
+      <p className="placeholder-text">This is the Privacy Policy page.</p>
+    </PageContainer>
+  );
+}
